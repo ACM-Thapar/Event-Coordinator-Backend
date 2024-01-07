d4620856af299ba1b607444283117d2311102797
gitvivek <varora2004@gmail.com>
2024-01-07 06:19:20 +0530

    event-structure_done

--- src/controllers/event/event.controller.ts
+++ src/controllers/event/event.controller.ts
@@ -2,19 +2,19 @@ import { Router, Request, Response, NextFunction } from "express";
 import eventModel from "./event.model";
 import Controller from "@interfaces/controller.interface";
 import { EventNotFoundException } from "@utils/exceptions/application.exception";
+import { title } from "process";
 
 class EventController implements Controller {
   public path = "/event";
   public router = Router();
   private event = eventModel;
-
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
     this.router.get(this.path, this.getAllEvents);
-    this.router.post(this.path, this.createEvent);
+    this.router.post(`${this.path}`, this.createEvent);
     this.router.put(`${this.path}/:id`, this.updateEvent);
     this.router.delete(`${this.path}/:id`, this.deleteEvent);
   }
@@ -39,11 +39,18 @@ class EventController implements Controller {
   ) => {
     try {
       const eventData: Event = request.body;
-      const createdEvent = new this.event(eventData);
-      const savedEvent = await createdEvent.save();
-      response.send(savedEvent);
+      type ObjectKey = keyof typeof eventData;
+      // Simple Check if Title is not Present.(till users are not there)
+      if(!eventData['title' as ObjectKey,'description' as ObjectKey]){
+        return response.status(400).json({
+          success:false,
+          message:"Missing Fields"
+        })
+      }
+      const createdEvent = await this.event.create(eventData);
+      response.send(createdEvent);
     } catch (error) {
-      next(error);
+      return  response.status(400).json({success: false,message:"Error in creating Event."})
     }
   };
 
@@ -72,9 +79,19 @@ class EventController implements Controller {
     try {
       const id = request.params.id;
       const successResponse = await this.event.findByIdAndDelete(id);
+      if(!successResponse){
+        return response.status(400).json({
+          status:"400",
+          message:"Event Not Found"
+        }) 
+      }
       response.send(successResponse);
     } catch (error) {
-      next(error);
+      return response.status(400).json({
+        status:"false",
+        message:"Unable to Delete the Event"
+      })
+      //next(error);
     }
   };
 }
